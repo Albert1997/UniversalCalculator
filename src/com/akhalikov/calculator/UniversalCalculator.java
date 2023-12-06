9f2502fc833ba723332256824d22436d7f620878
akhalikov <khalikovalbert@mail.ru>
2023-12-06 17:57:51 +0500

    Add readme file

--- src/com/akhalikov/calculator/UniversalCalculator.java
+++ src/com/akhalikov/calculator/UniversalCalculator.java
@@ -1,41 +1,135 @@
 package com.akhalikov.calculator;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class UniversalCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println(input);
-        checkInput(input);
-        String[] values = input.split(" ");
-        System.out.println(values);
-        if (values.length != 3) {
-            System.out.println("Неверное количество аргументов");
-        }
-        String operator = values[1];
+        Scanner scanner = new Scanner(System.in);
 
+
+        Map<Character, Integer> romanNumerals = new HashMap<>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+
+
+        System.out.print("Введите выражение: ");
+        String input = scanner.nextLine();
+
+
+        String[] tokens = input.split(" ");
+
+        if (tokens.length != 3) {
+            throw new IllegalArgumentException("Некорректный формат выражения");
+        }
+
+        String strNum1 = tokens[0];
+        String strNum2 = tokens[2];
+        String operation = tokens[1];
+
+
+        int num1, num2;
+        if (isRomanNumeral(strNum1) && isRomanNumeral(strNum2)) {
+            num1 = convertRomanToDecimal(strNum1, romanNumerals);
+            num2 = convertRomanToDecimal(strNum2, romanNumerals);
+        } else {
+            num1 = Integer.parseInt(strNum1);
+            num2 = Integer.parseInt(strNum2);
+
+
+            if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
+                throw new IllegalArgumentException("Числа должны быть от 1 до 10");
+            }
+        }
+
+
+        int result;
+
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Некорректная операция");
+        }
+
+
+        if (isRomanNumeral(strNum1) && isRomanNumeral(strNum2)) {
+            String romanResult = convertDecimalToRoman(result, romanNumerals);
+            System.out.println("Результат: " + romanResult);
+        } else {
+            System.out.println("Результат: " + result);
+
+
+        }
     }
 
-    private static void checkInput(String input) {
-        if (input.isEmpty()) {
-            System.out.println("Строка пустая");
-        } else {
-            System.out.println("Строка не пустая");
-        }
+
+    private static boolean isRomanNumeral(String input) {
+        return input.matches("[IVXLCDM]+");
     }
 
-    private static void checkOperator(String operator) {
-        if (operator.equals("+")) {
-            System.out.println("Сложение");
-        } else if (operator.equals("-")) {
-            System.out.println("Вычитание");
-        } else if (operator.equals("*")) {
-            System.out.println("Умножение");
-        } else if (operator.equals("/")) {
-            System.out.println("Деление");
-        } else {
-            System.out.println("Неверное выражение");
+
+    private static int convertRomanToDecimal(String input, Map<Character, Integer> romanNumerals) {
+        int result = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentNumeral = input.charAt(i);
+
+
+            if (!romanNumerals.containsKey(currentNumeral)) {
+                throw new IllegalArgumentException("Некорректная римская цифра: " + currentNumeral);
+            }
+
+            int currentValue = romanNumerals.get(currentNumeral);
+
+
+            if (i + 1 < input.length()) {
+                char nextNumeral = input.charAt(i + 1);
+                int nextValue = romanNumerals.get(nextNumeral);
+
+                if (nextValue > currentValue) {
+                    result -= currentValue;
+                } else {
+                    result += currentValue;
+                }
+            } else {
+                result += currentValue;
+            }
         }
+
+        return result;
+    }
+
+
+    private static String convertDecimalToRoman(int input, Map<Character, Integer> romanNumerals) {
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<Character, Integer> entry : romanNumerals.entrySet()) {
+            char numeral = entry.getKey();
+            int value = entry.getValue();
+
+            while (input >= value) {
+                result.append(numeral);
+                input -= value;
+            }
+        }
+
+        return result.toString();
     }
 }
+
+
+
+
